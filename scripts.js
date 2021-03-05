92c92382ad62235b1b45b9a72603c9973541ba22
Thestickman391 <stickman391@gmail.com>
2021-03-05 19:18:03 +1300

    asdasd

--- scripts.js
+++ scripts.js
@@ -1,5 +1,38 @@
+var loadedItems = 1;
+fitty('#infotext', {
+	minSize: 12,
+	maxSize: 100
+});
+
+$(function() {
+	$("div[data-src]").lazy({
+		chainable: false,
+		afterLoad: function(element) {
+			loadMore(element);
+		}
+	});
+})
+
+function loadMore(element) {
+	console.log("loaded ");
+	document.getElementById("buttons").innerHTML += element[0].innerHTML;
+	$(element).remove();
+	var newLazy = $('<div data-loader="ajax" data-src="buttons-' + (loadedItems++).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) +'.html"></div>');
+	$('body').append(newLazy);
+	$("div[data-src]").lazy({
+		chainable: false,
+		afterLoad: function(element) {
+			loadMore(element);
+		}
+	});
+}
+
 const infoDiv = document.getElementById("info");
 const infotext = document.getElementById("infotext");
+const word = "This is a large word";
+const wordHidden = word.replace(/\S/g, "_&nbsp");
+document.getElementById("word").innerHTML = wordHidden;
+
 function showDetails(button) {
 	infoDiv.style.display = "flex";
 	var name = button.getAttribute("data-name");
@@ -9,3 +42,11 @@ function showDetails(button) {
 function hideDetails() {
 	infoDiv.style.display = "none";
 }
+
+function guess(button) {
+	var guessedCodepoint = button.getAttribute("data-codepoint");
+	var guessedChar = (String.fromCodePoint(guessedCodepoint));
+	console.log(guessedChar);
+	console.log(word.match(new RegExp(guessedChar, 'g')));
+	// if()
+} 
